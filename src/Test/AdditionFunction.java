e7c01d4aee4cdcb6e3d7d481ff66f5924cec8987
jrkarthik87 <sqakarthi@gmail.com>
2023-12-20 08:26:50 +0530

    Initial Commit for this project

---
+++ src/Test/AdditionFunction.java
@@ -0,0 +1,15 @@
+package Test;
+
+public class AdditionFunction {
+    void Test()
+    {
+        int a = 2;
+        int b = 7;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+    public static void main(String[] args) {
+        AdditionFunction s = new AdditionFunction();
+        s.Test();
+    }
+}
